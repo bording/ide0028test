c984fcbd18903c312d8d668e73bd4a325cfd7720
Brandon Ording <bording@gmail.com>
2023-09-13 17:23:26 -0400

    Add broken and working examples

--- ide0028test/Test.cs
+++ ide0028test/Test.cs
@@ -1,12 +1,31 @@
 ﻿namespace ide0028test
 {
-    internal class Test
+    internal class Broken
     {
-        public Test()
+        public Broken()
         {
             Property = new List<int>();
         }
 
         public List<int>? Property { get; }
+
+        public void Method()
+        {
+            List<int> field = new List<int>();
+        }
+    }
+
+    internal class Working
+    {
+        public Working()
+        {
+        }
+
+        public List<int>? Property { get; } = new(); //no capacity required
+
+        public void Method()
+        {
+            List<int> field = new(100); //capacity required here
+        }
     }
 }
