772a9c2c5c7108bc99a4c2fb330c947121dc5892
Brandon Ording <bording@gmail.com>
2023-09-13 17:09:10 -0400

    Add stub

---
+++ ide0028test/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace ide0028test
+{
+    internal class Test
+    {
+        public Test()
+        {
+            Property = new List<int>();
+        }
+
+        public List<int>? Property { get; }
+    }
+}
